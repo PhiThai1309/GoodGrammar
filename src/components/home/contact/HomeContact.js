42983097d4c472fb6d24e1e7d753e2db9e2f956d
Phi Thai <thaimanhphi@gmail.com>
2023-12-03 22:11:00 +0700

    Done contact section

--- src/components/home/contact/HomeContact.js
+++ src/components/home/contact/HomeContact.js
@@ -1,10 +1,27 @@
-const HomeContact = () => {
-    return (
-        <div className="navbar-logo">
-            {/* <img id="logo" src={require("../../../assets/logo.png")} alt="" /> */}
-            <p className="logo">Good {<br />} Grammar</p>
-        </div>
-    )
-}
+import "./HomeContact.css";
 
-export default HomeContact
+const HomeContact = () => {
+  return (
+    <div className="contact_container">
+      <div className="contact_wrapper">
+        <p className="logo">Good {<br />} Grammar</p>
+        <div className="contact_form_wrapper">
+          <form>
+            <input
+              className="contact_form_input"
+              type="text"
+              id="fname"
+              name="fname"
+              placeholder="Leave your email address here!"
+            />
+          </form>
+          <button className="contact_form_submit">
+            <span class="material-symbols-rounded">arrow_forward</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeContact;
