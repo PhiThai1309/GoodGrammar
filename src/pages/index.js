eaf9bd0a6ef1deee9c54173b350f55f79d07186f
Phi Thai <thaimanhphi@gmail.com>
2023-12-04 12:53:16 +0700

    Updates

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './home/Home'
-export { default as Grammar } from './grammar/Grammar'
-export { default as History } from './history/History'
-export { default as PageNavigation } from './pageNavigation/PageNavigation'
