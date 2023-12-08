476ffda865162dc082ee58ce8049e426ac0b536a
Phi Thai <thaimanhphi@gmail.com>
2023-12-08 22:52:07 +0700

    Add Clerk

--- src/App.js
+++ src/App.js
@@ -2,20 +2,47 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import { useState } from "react";
 import { Home, PageNavigation } from "./pages";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
+
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="history" element={ <PageNavigation content="history"/>} />
-          <Route path="grammar" element={<PageNavigation content="grammar"/>} />
-          <Route path="subscribe" element={<PageNavigation content="subscribe" />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <SignedIn>
+        <Router />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </ClerkProvider>
   );
+}
+
+function Router() {
+  <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="history" element={<PageNavigation content="history" />} />
+        <Route path="grammar" element={<PageNavigation content="grammar" />} />
+        <Route
+          path="subscribe"
+          element={<PageNavigation content="subscribe" />}
+        />
+      </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
