682bd2f7783e67e1a81ee62e53e638d2deb6b4b6
Phi Thai <thaimanhphi@gmail.com>
2024-01-03 00:07:45 +0700

    Update UI Grammar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,37 +9,33 @@ const Navbar = () => {
     <nav>
       <div className="navbar-logo">
         <CustomLink href="/">
-          <p className="logo">Good {<br />} Grammar</p>
+          <p className="logo_small">Good {<br />} Grammar</p>
         </CustomLink>
       </div>
       <div className="navigation-container">
+        <CustomLink href="/">
+          <span className="material-symbols-rounded">home</span>
+          <span>Home</span>
+        </CustomLink>
         <CustomLink href="/grammar">
-          {/* <span className="material-symbols-rounded">spellcheck</span> */}
-          <span>Checker</span>
+          <span className="material-symbols-rounded">spellcheck</span>
+          <span>Grammar</span>
         </CustomLink>
         <CustomLink href="/history">
-          {/* <span className="material-symbols-rounded">history</span> */}
+          <span className="material-symbols-rounded">history</span>
           <span>History</span>
         </CustomLink>
-      </div>
-      <div className="subscribe">
         <CustomLink href="/subscribe">
-          {/* <span className="material-symbols-rounded">workspace_premium</span> */}
-          {/* <span>Subscription</span> */}
-          <button class="Btn">
-            <svg class="logoIcon" height="1em" viewBox="0 0 576 512">
-              <path d="M309 106c11.4-7 19-19.7 19-34c0-22.1-17.9-40-40-40s-40 17.9-40 40c0 14.4 7.6 27 19 34L209.7 220.6c-9.1 18.2-32.7 23.4-48.6 10.7L72 160c5-6.7 8-15 8-24c0-22.1-17.9-40-40-40S0 113.9 0 136s17.9 40 40 40c.2 0 .5 0 .7 0L86.4 427.4c5.5 30.4 32 52.6 63 52.6H426.6c30.9 0 57.4-22.1 63-52.6L535.3 176c.2 0 .5 0 .7 0c22.1 0 40-17.9 40-40s-17.9-40-40-40s-40 17.9-40 40c0 9 3 17.3 8 24l-89.1 71.3c-15.9 12.7-39.5 7.5-48.6-10.7L309 106z"></path>
-            </svg>
-            Go Premium
-            {/* <span class="tooltip">Click here!!</span> */}
-          </button>
+          <span className="material-symbols-rounded">workspace_premium</span>
+          <span>Subscription</span>
         </CustomLink>
       </div>
+
       <div className="profile-container">
         {/* <span className="material-symbols-rounded">help</span> */}
         <div className="user_btn">
           <UserButton afterSignOutUrl="/" />
-          {/* <span>Account</span> */}
+          <span>Account</span>
         </div>
       </div>
     </nav>
@@ -51,7 +47,7 @@ function CustomLink({ href, children, ...props }) {
   return (
     <a
       href={href}
-      className={"nav-item" + (path === href ? " nav_selected" : "")}
+      className={"nav-item" + (path === href ? " selected" : "")}
       {...props}
     >
       {children}
--- src/pages/grammar/Grammar.jsx
+++ src/pages/grammar/Grammar.jsx
@@ -1,5 +1,8 @@
 import React, { useLayoutEffect, useRef, useState } from "react";
 import "./Grammar.css";
+import Mouse from "../../components/mouse/Mouse";
+import placeholder from "../../assets/placeholder1.png";
+import { HomeFooter } from "../../components";
 
 const Grammar = (props) => {
   const [model, setModel] = useState("standard");
@@ -36,121 +39,247 @@ const Grammar = (props) => {
     <div className="grammar">
       {/* <h2>Grammar Checker</h2> */}
 
-      <div className="model">
-        <h3 className="green_text">Models:</h3>
-        <ul className="model-menu">
-          <li
-            onClick={() => {
-              setModel("standard");
-            }}
-            className={model === "standard" ? "selected" : null}
-          >
-            Standard
-          </li>
-          <li
-            onClick={() => {
-              setModel("formal");
-            }}
-            className={model === "formal" ? "selected" : null}
-          >
-            Formal
-          </li>
-          <li
-            onClick={() => {
-              setModel("academic");
-            }}
-            className={model === "academic" ? "selected" : null}
-          >
-            Academic
-          </li>
-          <li
-            onClick={() => {
-              setModel("simple");
-            }}
-            className={model === "simple" ? "selected" : null}
-          >
-            Simple
-          </li>
-        </ul>
+      <div className="header">
+        <h3>Grammar Checker</h3>
       </div>
-      <div className="content">
-        <div className="content-left">
-          <div
-            className="textarea"
-            contentEditable="true"
-            placeholder="Enter text here or import word file"
-          ></div>
-          <div className="input-buttons">
-            <button className="icon-button" onClick={clickDelete}>
-              <span className="material-symbols-rounded">delete</span>
-            </button>
-            <input
-              type="file"
-              id="file"
-              onChange={clickUpload}
-              accept=".doc, .docx, .txt, .pdf"
-            ></input>
-            <label htmlFor="file" className="icon-button">
-              <span className="material-symbols-rounded">upload_file</span>
-            </label>
-            <button className="filled-btn orange" onClick={clickParaphrase}>
-              Check
-            </button>
-          </div>
-        </div>
 
-        <div className="content-right">
-          <textarea
-            name="text"
-            placeholder="Corrected text will be shown here"
-            disabled
-          ></textarea>
-          <div className="input-buttons">
-            {/* <button className="filled-btn orange" onClick={clickDownload}>
+      <div className="content_wrapper">
+        <div className="model">
+          <h3 className="green_text">Models:</h3>
+          <ul className="model-menu">
+            <li
+              onClick={() => {
+                setModel("standard");
+              }}
+              className={model === "standard" ? "selected" : null}
+            >
+              Standard
+            </li>
+            <li
+              onClick={() => {
+                setModel("formal");
+              }}
+              className={model === "formal" ? "selected" : null}
+            >
+              Formal
+            </li>
+            <li
+              onClick={() => {
+                setModel("academic");
+              }}
+              className={model === "academic" ? "selected" : null}
+            >
+              Academic
+            </li>
+            <li
+              onClick={() => {
+                setModel("simple");
+              }}
+              className={model === "simple" ? "selected" : null}
+            >
+              Simple
+            </li>
+          </ul>
+        </div>
+        <div className="content">
+          <div className="content-left">
+            <div
+              className="textarea"
+              contentEditable="true"
+              placeholder="Enter text here or import word file"
+            ></div>
+            <div className="input-buttons">
+              <button className="icon-button" onClick={clickDelete}>
+                <span className="material-symbols-rounded">delete</span>
+              </button>
+              <input
+                type="file"
+                id="file"
+                onChange={clickUpload}
+                accept=".doc, .docx, .txt, .pdf"
+              ></input>
+              <label htmlFor="file" className="icon-button">
+                <span className="material-symbols-rounded">upload_file</span>
+              </label>
+              <button className="filled-btn orange" onClick={clickParaphrase}>
+                Check
+              </button>
+            </div>
+          </div>
+
+          <div className="content-right">
+            <textarea
+              name="text"
+              placeholder="Corrected text will be shown here"
+              disabled
+            ></textarea>
+            <div className="input-buttons">
+              {/* <button className="filled-btn orange" onClick={clickDownload}>
               Download
             </button> */}
-            <button className="download-button">
-              <div className="docs filled-btn orange">
-                <svg
-                  className="css-i6dzq1"
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  fill="none"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  height="20"
-                  width="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                  <polyline points="14 2 14 8 20 8"></polyline>
-                  <line y2="13" x2="8" y1="13" x1="16"></line>
-                  <line y2="17" x2="8" y1="17" x1="16"></line>
-                  <polyline points="10 9 9 9 8 9"></polyline>
-                </svg>{" "}
-                Download
-              </div>
-              <div className="download">
-                <svg
-                  className="css-i6dzq1"
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  fill="none"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  height="24"
-                  width="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                  <polyline points="7 10 12 15 17 10"></polyline>
-                  <line y2="3" x2="12" y1="15" x1="12"></line>
-                </svg>
-              </div>
-            </button>
+              <button className="download-button">
+                <div className="docs filled-btn orange">
+                  <svg
+                    className="css-i6dzq1"
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    fill="none"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    height="20"
+                    width="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                    <polyline points="14 2 14 8 20 8"></polyline>
+                    <line y2="13" x2="8" y1="13" x1="16"></line>
+                    <line y2="17" x2="8" y1="17" x1="16"></line>
+                    <polyline points="10 9 9 9 8 9"></polyline>
+                  </svg>{" "}
+                  Download
+                </div>
+                <div className="download">
+                  <svg
+                    className="css-i6dzq1"
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    fill="none"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    height="24"
+                    width="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                    <polyline points="7 10 12 15 17 10"></polyline>
+                    <line y2="3" x2="12" y1="15" x1="12"></line>
+                  </svg>
+                </div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
+
+      <Mouse></Mouse>
+
+      <div id="below">
+        <div className="grammar_intro">
+          <h2>
+            Introducing <h2>AI Grammar Checker</h2>
+          </h2>
+          <h3 className="right_align">
+            a <h3> {" smart "} </h3> companion for flawless writing.
+          </h3>
+          <p>
+            Meet your writing ally, the AI Grammar Checker app! Unlock precision
+            and polish in every sentence effortlessly. Bid farewell to
+            grammatical errors and welcome a new era of refined communication.
+            Whether it's crafting emails, essays, or creative pieces, let our
+            intelligent tool enhance your writing, ensuring clarity and
+            correctness. Experience the confidence that comes with error-free
+            expression, thanks to the seamless assistance of our AI Grammar
+            Checker."
+          </p>
+        </div>
+        <div className="grammar_features_container">
+          <h2>Why use AI Grammar Checker?</h2>
+          <div className="grammar_features">
+            <svg
+              width="200"
+              height="200"
+              viewBox="0 0 200 200"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {" "}
+              <g clip-path="url(#clip0_221_10)">
+                {" "}
+                <path
+                  d="M0 0H100C155.228 0 200 44.7715 200 100V200H100C44.7715 200 0 155.228 0 100V0Z"
+                  fill="url(#paint0_linear_221_10)"
+                />{" "}
+              </g>{" "}
+              <defs>
+                {" "}
+                <linearGradient
+                  id="paint0_linear_221_10"
+                  x1="100"
+                  y1="0"
+                  x2="100"
+                  y2="200"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  {" "}
+                  <stop stop-color="#A7B5FF" />{" "}
+                  <stop offset="1" stop-color="#F3ACFF" />{" "}
+                </linearGradient>{" "}
+                <clipPath id="clip0_221_10">
+                  {" "}
+                  <rect width="200" height="200" fill="white" />{" "}
+                </clipPath>{" "}
+              </defs>{" "}
+            </svg>
+            <div className="simple_flex grammar_block">
+              <div className="item_1 flex_common_vertical">
+                <h3>Support with different language model</h3>
+                <img src={placeholder} />
+              </div>
+              <div className="item_2 flex_common_vertical">
+                <h3>Artificial Intelligent</h3>
+                <img />
+              </div>
+            </div>
+            <div className="simple_flex grammar_block">
+              <div className="item_2 flex_common_vertical">
+                <h3>Support doc, docx and pdf format</h3>
+                <img />
+              </div>
+              <div className="item_1 flex_common_vertical">
+                <h3>Support with different language model</h3>
+                <img src={placeholder} />
+              </div>
+            </div>
+            <svg
+              width="200"
+              height="200"
+              viewBox="0 0 200 200"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {" "}
+              <g clip-path="url(#clip0_104_40)">
+                {" "}
+                <path
+                  d="M100.254 200C97.0998 200 94.4337 197.716 93.6699 194.656C91.2352 184.903 86.5744 174.531 79.6875 163.542C71.5278 150.347 59.8958 138.108 44.7917 126.823C31.6549 116.894 18.5181 110.123 5.38138 106.511C2.27005 105.656 0 102.897 0 99.6702V99.6702C0 96.5066 2.18273 93.7795 5.22473 92.9109C18.1045 89.2335 30.5122 83.2631 42.4479 75C56.1632 65.4514 67.6215 53.993 76.8229 40.625C84.9629 28.7165 90.5507 16.9388 93.5863 5.29207C94.3815 2.24095 97.0676 0 100.221 0V0C103.409 0 106.114 2.29058 106.89 5.3833C108.642 12.3654 111.38 19.512 115.104 26.8229C119.792 35.8507 125.781 44.5312 133.073 52.8646C140.538 61.0243 148.872 68.4028 158.073 75C170.097 83.5231 182.32 89.5111 194.744 92.964C197.792 93.8109 200 96.5244 200 99.6874V99.6874C200 102.898 197.725 105.634 194.629 106.483C186.754 108.642 178.648 112.124 170.313 116.927C160.243 122.83 150.868 129.861 142.187 138.021C133.507 146.007 126.389 154.427 120.833 163.281C113.933 174.293 109.267 184.745 106.837 194.639C106.083 197.708 103.413 200 100.254 200V200Z"
+                  fill="url(#paint0_linear_104_40)"
+                />{" "}
+              </g>{" "}
+              <defs>
+                {" "}
+                <linearGradient
+                  id="paint0_linear_104_40"
+                  x1="27.5"
+                  y1="19"
+                  x2="149"
+                  y2="174.5"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  {" "}
+                  <stop stop-color="#FFD9A0" />{" "}
+                  <stop offset="1" stop-color="#FFF5F1" />{" "}
+                </linearGradient>{" "}
+                <clipPath id="clip0_104_40">
+                  {" "}
+                  <rect width="200" height="200" fill="white" />{" "}
+                </clipPath>{" "}
+              </defs>{" "}
+            </svg>
+          </div>
+        </div>
+      </div>
+
+      <HomeFooter disable="true" />
     </div>
   );
 };
