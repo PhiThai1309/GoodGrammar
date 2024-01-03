5f847ec086062cb9b9826cafb4d327528b73b479
Phi Thai <thaimanhphi@gmail.com>
2024-01-03 17:38:55 +0700

    Improve UI for History

--- src/pages/grammar/Grammar.jsx
+++ src/pages/grammar/Grammar.jsx
@@ -3,6 +3,7 @@ import "./Grammar.css";
 import Mouse from "../../components/mouse/Mouse";
 import placeholder from "../../assets/placeholder1.png";
 import { HomeFooter } from "../../components";
+import DownloadBtn from "../../components/downloadBtn/DownloadBtn";
 
 const Grammar = (props) => {
   const [model, setModel] = useState("standard");
@@ -114,48 +115,7 @@ const Grammar = (props) => {
               disabled
             ></textarea>
             <div className="input-buttons">
-              {/* <button className="filled-btn orange" onClick={clickDownload}>
-              Download
-            </button> */}
-              <button className="download-button">
-                <div className="docs filled-btn orange">
-                  <svg
-                    className="css-i6dzq1"
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    fill="none"
-                    strokeWidth="2"
-                    stroke="currentColor"
-                    height="20"
-                    width="20"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                    <polyline points="14 2 14 8 20 8"></polyline>
-                    <line y2="13" x2="8" y1="13" x1="16"></line>
-                    <line y2="17" x2="8" y1="17" x1="16"></line>
-                    <polyline points="10 9 9 9 8 9"></polyline>
-                  </svg>{" "}
-                  Download
-                </div>
-                <div className="download">
-                  <svg
-                    className="css-i6dzq1"
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    fill="none"
-                    strokeWidth="2"
-                    stroke="currentColor"
-                    height="24"
-                    width="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                    <polyline points="7 10 12 15 17 10"></polyline>
-                    <line y2="3" x2="12" y1="15" x1="12"></line>
-                  </svg>
-                </div>
-              </button>
+              <DownloadBtn class="orange" />
             </div>
           </div>
         </div>
@@ -168,7 +128,7 @@ const Grammar = (props) => {
           <h2>
             Introducing <b>AI Grammar Checker</b>
           </h2>
-          <h3 className="right_align" >
+          <h3 className="right_align">
             a <b> {"smart"} </b> companion for flawless writing.
           </h3>
           <p>
--- src/pages/history/History.jsx
+++ src/pages/history/History.jsx
@@ -1,30 +1,63 @@
-import React, { useState, useEffect } from 'react'
-import { Document } from '../../components'
-import './History.css'
+import React, { useState, useEffect } from "react";
+import { Document } from "../../components";
+import "./History.css";
+import DownloadBtn from "../../components/downloadBtn/DownloadBtn";
 
 const History = () => {
-    const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState(null);
 
-    // Use for updating document ig
-    useEffect(() => console.log(selected));
+  // Use for updating document ig
+  useEffect(() => console.log(selected));
 
-    return (
-        <div className='history'>
-            <div className='history-bar'>
-                <h1>History</h1>
-                <h2>Today</h2>
-                <Document id='1' title='Document #1' onClick={e => setSelected(e.target.id)}/>
-                <Document id='2' title='Document #2' onClick={e => setSelected(e.target.id)}/>
-                <Document id='3' title='Document #3' onClick={e => setSelected(e.target.id)}/>
+  return (
+    <div className="history_container">
+      <div className="header">
+        <h3>History</h3>
+      </div>
+      <div className="history">
+        <div className="history-bar">
+          <div className="history_document_container">
+            <h4>Today</h4>
+            <div className="history_document">
+              <Document
+                id="1"
+                title="Document #11231231231231231231231231"
+                onClick={(e) => setSelected(e.target.id)}
+              />
+              <Document
+                id="2"
+                title="Document #2"
+                onClick={(e) => setSelected(e.target.id)}
+              />
+              <Document
+                id="3"
+                title="Document #3"
+                onClick={(e) => setSelected(e.target.id)}
+              />
             </div>
-            <div className='history-text'>
-                <textarea placeholder='Corrected text will be shown here' disabled></textarea>
-                <div className="input-buttons">
-                    <button className='download-button'>Download</button>
-                </div>
-            </div>
+          </div>
+
+          <div>
+            <h3>Quick tips</h3>
+            <p>
+              Harness the precision of our Grammar AI to effortlessly perfect
+              your writing
+            </p>
+            <button />
+          </div>
         </div>
-    )
-}
+        <div className="history-text">
+          <textarea
+            placeholder="Corrected text will be shown here"
+            disabled
+          ></textarea>
+          <div className="input-buttons">
+            <DownloadBtn class="green" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default History
+export default History;
