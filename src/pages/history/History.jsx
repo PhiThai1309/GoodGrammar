b028a5258085ee7d6c727ea7175fc1f1eaff7bf7
Phi Thai <thaimanhphi@gmail.com>
2024-01-17 00:17:42 +0700

    Revert "Add dragable div for history"
    This reverts commit 1b3f9c5efef1f925817041475a9f1352d949ac6a.

--- src/pages/history/History.jsx
+++ src/pages/history/History.jsx
@@ -15,7 +15,7 @@ const History = (props) => {
   const [file, setFile] = useState(null); // download file
   const [loading, setLoading] = useState(true);
 
-  const fetchData = async () => {
+  const fetchData = async () => {      
     try {
       const token = await getToken();
       const response = await axios.get(API.history(), {
@@ -24,7 +24,7 @@ const History = (props) => {
         },
       });
       console.log(response.data);
-      const reverseList = response.data.reverse();
+      const reverseList = response.data.reverse()
       setHistory(response.data);
       setLoading(false);
     } catch (error) {
@@ -45,12 +45,8 @@ const History = (props) => {
             key={index}
             id={index}
             item={item}
-            documentClick={() => {
-              documentClick(item);
-            }}
-            deleteClick={() => {
-              deleteClick(item);
-            }}
+            documentClick={() => { documentClick(item); }}
+            deleteClick={() => { deleteClick(item); }}
             selectedId={selectedId}
           />
         ))}
@@ -63,18 +59,10 @@ const History = (props) => {
     setSelectedId(item.file_id);
     // console.log(item.file_id);
 
-    const response = await axios
-      .get(API.getFileInfo(item.file_id))
-      .catch((err) => {
-        console.error(err);
-      });
+    const response = await axios.get(API.getFileInfo(item.file_id)).catch((err) => { console.error(err); });
     setText(response.data.content);
 
-    const file = await axios
-      .get(API.getFile(item.file_id), { responseType: "blob" })
-      .catch((err) => {
-        console.error(err);
-      });
+    const file = await axios.get(API.getFile(item.file_id), { responseType: "blob" }).catch((err) => { console.error(err); });
     setFile(file.data);
   };
 
@@ -94,15 +82,11 @@ const History = (props) => {
       })
       .then((res) => {
         // console.log(res.data);
-        const newHistory = history.filter(
-          (file) => file.file_id !== item.file_id
-        );
-        setHistory(newHistory);
+        const newHistory = history.filter((file) => file.file_id !== item.file_id);
+        setHistory(newHistory);    
       })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+      .catch((err) => { console.error(err); })
+  }
 
   const downloadClick = () => {
     // Create download link for the file
@@ -113,111 +97,71 @@ const History = (props) => {
     link.click();
   };
 
-  const [width, setWidth] = useState(300);
-  const [mouseDown, setMouseDown] = useState(false);
-
-  const handleMouseDown = (event) => {
-    setMouseDown(true);
-    event.preventDefault();
-  };
-
-  const handleMouseUp = (_event) => {
-    setMouseDown(false);
-  };
-
-  const getWindowDimensions = () => {
-    const { innerWidth: width, innerHeight: height } = window;
-    return { width, height };
-  };
-
-  const handleMouseMove = (event) => {
-    if (mouseDown) {
-      // check if the remaining width is < 250px comapred to window size
-      if (getWindowDimensions().width - event.clientX >= 300) {
-        setWidth(event.clientX - 108);
-      }
-    }
-  };
-
-  if (loading) return <Loading />;
+  if (loading) return <Loading />
 
   return (
-    <div
-      className="history_container"
-      onMouseUp={handleMouseUp}
-      onMouseMove={handleMouseMove}
-    >
+    <div className="history_container">
+      <div className="header">
+        <h3>History</h3>
+        {showSub(props.sub)}
+      </div>
       <div className="history">
-        <div className="simple_flex history_left">
-          <div className="header green">
-            <h3>History</h3>
-            {showSub(props.sub)}
+        <div className="history-bar">
+          <div className="history_document_container">
+            <h4>Today</h4>
+            <div className="history_document">
+              <DocumentList list={history} selected={selected} />
+            </div>
           </div>
-          <div className="simple_flex flex_grow">
-            <div className="history-bar" style={{ width: width }}>
-              <div className="history_document_container">
-                <h4>Today</h4>
-                <DocumentList list={history} selected={selected} />
-              </div>
-              <a href="grammar">
-                <div className="tips_card">
-                  <h3>Quick tips</h3>
-                  <p className="paragraph">
-                    Harness the precision of our Grammar AI to effortlessly
-                    perfect your writing
-                  </p>
-                  <button className="circle_btn_small"> Learn more </button>
-                  <svg
-                    id="sw-js-blob-svg"
-                    viewBox="0 0 100 100"
-                    xmlns="http://www.w3.org/2000/svg"
+          <a href="grammar">
+            <div className="tips_card">
+              <h3>Quick tips</h3>
+              <p className="paragraph">
+                Harness the precision of our Grammar AI to effortlessly
+                perfect your writing
+              </p>
+              <button className="circle_btn_small"> Learn more </button>
+              <svg
+                id="sw-js-blob-svg"
+                viewBox="0 0 100 100"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                {" "}
+                <defs>
+                  {" "}
+                  <linearGradient
+                    id="sw-gradient"
+                    x1="0"
+                    x2="1"
+                    y1="1"
+                    y2="0"
                   >
                     {" "}
-                    <defs>
-                      {" "}
-                      <linearGradient
-                        id="sw-gradient"
-                        x1="0"
-                        x2="1"
-                        y1="1"
-                        y2="0"
-                      >
-                        {" "}
-                        <stop
-                          id="stop1"
-                          stopColor="rgba(27, 71, 21, 0.4)"
-                          offset="0%"
-                        ></stop>{" "}
-                        <stop
-                          id="stop2"
-                          stopColor="rgba(165.212, 239.721, 155.052, 0.64)"
-                          offset="80%"
-                        ></stop>{" "}
-                      </linearGradient>{" "}
-                    </defs>{" "}
-                    <path
-                      fill="url(#sw-gradient)"
-                      d="M28.7,-31.1C36.5,-27.6,41.8,-18,41.8,-8.7C41.9,0.5,36.6,9.3,30.8,16.3C25,23.3,18.6,28.5,11.6,30.4C4.6,32.4,-3.1,31,-10.7,28.6C-18.3,26.2,-25.9,22.7,-31.9,16.4C-37.9,10.1,-42.3,1,-41.2,-7.6C-40.1,-16.2,-33.5,-24.2,-25.8,-27.7C-18,-31.2,-9,-30.3,0.7,-31.1C10.4,-32,20.8,-34.6,28.7,-31.1Z"
-                      width="100%"
-                      height="100%"
-                      transform="translate(80 60)"
-                      strokeWidth="0"
-                      stroke="url(#sw-gradient)"
-                    ></path>{" "}
-                  </svg>
-                </div>
-              </a>
+                    <stop
+                      id="stop1"
+                      stopColor="rgba(27, 71, 21, 0.4)"
+                      offset="0%"
+                    ></stop>{" "}
+                    <stop
+                      id="stop2"
+                      stopColor="rgba(165.212, 239.721, 155.052, 0.64)"
+                      offset="80%"
+                    ></stop>{" "}
+                  </linearGradient>{" "}
+                </defs>{" "}
+                <path
+                  fill="url(#sw-gradient)"
+                  d="M28.7,-31.1C36.5,-27.6,41.8,-18,41.8,-8.7C41.9,0.5,36.6,9.3,30.8,16.3C25,23.3,18.6,28.5,11.6,30.4C4.6,32.4,-3.1,31,-10.7,28.6C-18.3,26.2,-25.9,22.7,-31.9,16.4C-37.9,10.1,-42.3,1,-41.2,-7.6C-40.1,-16.2,-33.5,-24.2,-25.8,-27.7C-18,-31.2,-9,-30.3,0.7,-31.1C10.4,-32,20.8,-34.6,28.7,-31.1Z"
+                  width="100%"
+                  height="100%"
+                  transform="translate(80 60)"
+                  strokeWidth="0"
+                  stroke="url(#sw-gradient)"
+                ></path>{" "}
+              </svg>
             </div>
-            <div
-              className="resize"
-              style={{ width: "10px" }}
-              onMouseDown={handleMouseDown}
-            >
-              <span class="material-symbols-rounded">more_vert</span>
-            </div>
-          </div>
+          </a>
         </div>
-
         <div className="history-text">
           <textarea
             placeholder="Corrected text will be shown here"
