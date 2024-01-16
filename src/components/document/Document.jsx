e8ea995b62b48994670af69750093c61b5d6d4fd
Phi Thai <thaimanhphi@gmail.com>
2024-01-17 00:28:25 +0700

    change delete icon

--- src/components/document/Document.jsx
+++ src/components/document/Document.jsx
@@ -4,7 +4,10 @@ import "./Document.css";
 const Document = (props) => {
   return (
     <div
-      className={"history-item" + (props.selectedId === props.item.file_id ? " selected" : "")}
+      className={
+        "history-item" +
+        (props.selectedId === props.item.file_id ? " selected" : "")
+      }
       id={props.id}
     >
       <div className="document-info flex_common" onClick={props.documentClick}>
@@ -12,7 +15,7 @@ const Document = (props) => {
         <p>{props.item.file_name}</p>
       </div>
       <button className="simple-button" onClick={props.deleteClick}>
-        <span className="material-symbols-rounded">more_vert</span>
+        <span class="material-symbols-rounded">delete</span>
       </button>
     </div>
   );
