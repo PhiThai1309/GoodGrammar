9f9665e251eb2e70f96fcac74b0089aa25cbc9f0
Phi Thai <thaimanhphi@gmail.com>
2024-01-17 23:08:56 +0700

    update

--- src/components/document/Document.jsx
+++ src/components/document/Document.jsx
@@ -12,7 +12,7 @@ const Document = (props) => {
     >
       <div className="document-info flex_common" onClick={props.documentClick}>
         <span className="material-symbols-rounded">description</span>
-        <p>{props.item.file_name}</p>
+        <p className="paragraph">{props.item.file_name}</p>
       </div>
       <button className="simple-button" onClick={props.deleteClick}>
         <span className="material-symbols-rounded">delete</span>
